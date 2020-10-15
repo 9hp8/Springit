5fee0e55849c9f58b216ab37905ffdbf209a3ac6
hpadmin <hpadmin@DESKTOP-R53V40A>
2020-10-15 22:38:13 +0530

    Initial commit for our spring boot reddit clone

---
+++ springit/src/test/java/com/hp/springit/SpringGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hp.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
